e42a28428fe5042a9396138220679014c1047b46
Alex Tyurin <alexturin@gmail.com>
2018-07-07 15:34:30 +0700

    Второе занятие

--- src/index.js
+++ src/index.js
@@ -4,32 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function Hello () {
-	let phrase = "World"
-	return (
-			<h1>Hello {phrase} </h1>
-		)
-}
 
-function SayForName (props) {
-	return (
-		<div>
-			<h1> Мое имя {props.name}, фамилия  - {props.surname}</h1>
-			<a href={props.link}> Ссылка на мой профиль </a>
-		</div>
-		)
-}
-
-function MeetAll () {
-	return (
-			<div>
-				<SayForName name="Alex" surname="Tyurin" link="vk.com" />
-				<SayForName name="Irina" surname="Tyurina" link="vk.com" />
-				<SayForName name="Valrntina" surname="Tyurin" link="vk.com" />
-			</div>
-		)
-}
-
-
-ReactDOM.render(<MeetAll />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
